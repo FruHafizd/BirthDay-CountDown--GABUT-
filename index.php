09f21ea09d0ed7d2fe3e5f2880531d5d50574801
FruHafizd <muhammadhafidz12333@gmail.com>
2024-11-14 10:07:20 +0800

    Adedd

--- index.php
+++ index.php
@@ -249,7 +249,6 @@ $photos = [
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.3/js/lightbox.min.js"></script>
     <script>
-        // [Previous JavaScript code remains the same]
         // Create stars
         function createStars() {
             const starsContainer = document.getElementById('stars');
